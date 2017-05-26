b9216ec73a37d233f05a6cd8a6f128cfedc9ad76
Kalist <kalisture@gmail.com>
2017-05-26 14:46:39 +1000

    Haven't done incorrect data type handling just yet. in the meantime, made it so users can't input repeat values, or data outside the range, with appropriate error messages, and otehr various code cleanups.

--- poll.c
+++ poll.c
@@ -16,9 +16,10 @@ typedef struct user user_t;
 void printMenu(void);
 void voteNow(void);
 void printParty(void);
-void printPref(int choice);
+void printPref(int choice, int pos);
 /*Prints the users preferences*/
-int getPrefs(int* p);
+void getPrefs(int* p);
+int checkpref(int* p, int position);
 
 int main(void)
 {
@@ -48,9 +49,7 @@ void voteNow()
 	printf("1. I am voing above the line\n");
 	printf("2. I am voting below the line\n");*/
 	printf("You are voting in the Sydney electorate.\n");
-	printf("Below are the parties you may choose from:\n");
 	printParty();
-	printf("Please make your selection by enetering your preferences in order\n");
 	int* p = user.above;
 	getPrefs(p);
 	
@@ -61,8 +60,8 @@ void voteNow()
 	printf("Your preferences are from highest to lowest as follows:\n");
 	for(i=0;i<=MAX_PREFERENCES;i++)
 	{	
-		printf("\n>");
-		printPref(user.above[i]);
+		printf("\n");
+		printPref(user.above[i], i);
 	}
 	/*prints the users preferences*/
 
@@ -73,13 +72,14 @@ void voteNow()
 		scanf("%d", &areyousure);
 		if(areyousure != 1)
 		{
-			printf("Please make your selection by enetering your preferences in order\n");
+			printf("\n");
+			printParty();
 			getPrefs(p);
 			printf("Your preferences are from highest to lowest as follows:\n");
 			for(i=0;i<=MAX_PREFERENCES;i++)
 			{	
-				printf("\n>");
-				printPref(user.above[i]);
+				printf("\n");
+				printPref(user.above[i], i);
 			}
 		}
 		else
@@ -87,12 +87,11 @@ void voteNow()
 			areyousure = 0;
 		}
 	}
-	printf("Thank you for voting");
-
-	
+	printf("Thank you for voting\n");
 }
 void printParty()
 {
+	printf("Below are the parties you may choose from:\n");
 	printf("1. Labor\n");
 	printf("2. Coalition\n");		
 	printf("3. Greens\n");
@@ -102,55 +101,93 @@ void printParty()
 	printf("7. Science Party\n");		
 	printf("8. Australian Sex Party\n");		
 	printf("9. Sustainable Australia\n");		
-	printf("10. Online Direct Democracy\n");		
+	printf("10. Online Direct Democracy\n");	
+	printf("Please make your selection by enetering your preferences in order\n");	
 }
 
 
-void printPref(int choice)
+void getPrefs(int* p)
+{
+	int i=0;
+
+	for(i=0;i<MAX_PREFERENCES;i++)
+		{
+			printf(">");
+			scanf("%d", &p[i]);
+			if (checkpref(p, i) != 0)
+			{
+				printf("Please make a new choice\n");
+				i--;
+			}
+		}
+
+}
+
+int checkpref(int* p, int position)
+{
+	if(MAX_PREFERENCES < p[position] || p[position] <= 0)
+	{
+		printf("Error; please enter an integer within the range.\n");
+		return 1;
+	}
+	int i=0;
+	for(i=0;i<position;i++)
+	{
+		if (p[position] == p[i])
+		{
+			printf("Error; you have already selected that party.\n");
+			return 1;
+		}
+	}
+	return 0;
+}
+
+
+void printPref(int choice, int pos)
 {
 	switch(choice)
 	{
 		case 1:
+			printf("%d. ", (pos+1));
 			printf("Labor");
 			break;
 		case 2:
+			printf("%d. ", (pos+1));
 			printf("Coalition");
 			break;
 		case 3:
+			printf("%d. ", (pos+1));
 			printf("Greens");
 			break;
 		case 4:
+			printf("%d. ", (pos+1));
 			printf("Christian Democrats Party");
 			break;
 		case 5:
+			printf("%d. ", (pos+1));
 			printf("Animal Justice Party");
 			break;
 		case 6:
+			printf("%d. ", (pos+1));
 			printf("Socialist Allience");
 			break;
 		case 7:
+			printf("%d. ", (pos+1));
 			printf("Science Party");
 			break;
 		case 8:
+			printf("%d. ", (pos+1));
 			printf("Australian Sex Party");
 			break;
 		case 9:
+			printf("%d. ", (pos+1));
 			printf("Sustainable Australia");
 			break;
 		case 10:
+			printf("%d. ", (pos+1));
 			printf("Online Direct Democracy");
 			break;
 	}
 }
 
 
-int getPrefs(int* p)
-{
-	int i=0;
-	for(i=0;i<MAX_PREFERENCES;i++)
-		{
-			printf(">");
-			scanf("%d", &p[i]);
-		}
-	return 1;
-}
